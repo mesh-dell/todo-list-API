66c0c0ba66b341e611ee00836b2a7fac0fd16524
mesh-dell <mwaimonicas@gmail.com>
2025-12-07 01:13:52 +0300

    feat: add config package with GetConfig function for environment variable management

---
+++ config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DbAddr     string
+	DbName     string
+	DbUser     string
+	DbPassword string
+	Port       string
+}
+
+func GetConfig() (Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return Config{}, err
+	}
+	c := Config{
+		DbAddr:     os.Getenv("DB_ADDR"),
+		DbName:     os.Getenv("DB_NAME"),
+		DbUser:     os.Getenv("DB_USER"),
+		DbPassword: os.Getenv("DB_PASSWORD"),
+		Port:       os.Getenv("PORT"),
+	}
+
+	if c.Port == "" {
+		c.Port = "8080"
+	}
+
+	if c.DbAddr == "" {
+		c.DbAddr = "localhost:3306"
+	}
+
+	if c.DbName == "" || c.DbUser == "" || c.DbPassword == "" {
+		return c, fmt.Errorf("missing dbName, dbUser and dbPassword environment variables")
+	}
+
+	return c, nil
+}
